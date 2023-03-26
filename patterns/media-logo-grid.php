7a7902822629d45ffcf1da4625734e0805930a44
Anders <anders@andersnoren.se>
2023-03-26 11:52:48 +0200

    Style variations

---
+++ patterns/media-logo-grid.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * Title: Grid with logotypes
+ * Slug: abisko/media-logo-grid
+ * Categories: abisko, abisko-media
+ */
+?>
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide">
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+	<div class="wp-block-columns alignwide">
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"width":"0px","style":"none"}}} -->
+		<div class="wp-block-column"
+			style="border-style:none;border-width:0px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+			<!-- wp:image {"align":"center","width":48,"linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter is-resized"><img
+					src="http://localhost:8888/utsikt/wp-content/themes/abisko/assets/images/icons/cube.png" alt=""
+					width="48" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"width":"0px","style":"none"}}} -->
+		<div class="wp-block-column"
+			style="border-style:none;border-width:0px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+			<!-- wp:image {"align":"center","width":48,"linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter is-resized"><img
+					src="http://localhost:8888/utsikt/wp-content/themes/abisko/assets/images/icons/cube.png" alt=""
+					width="48" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"width":"0px","style":"none"}}} -->
+		<div class="wp-block-column"
+			style="border-style:none;border-width:0px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+			<!-- wp:image {"align":"center","width":48,"linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter is-resized"><img
+					src="http://localhost:8888/utsikt/wp-content/themes/abisko/assets/images/icons/cube.png" alt=""
+					width="48" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"width":"0px","style":"none"}}} -->
+		<div class="wp-block-column"
+			style="border-style:none;border-width:0px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+			<!-- wp:image {"align":"center","width":48,"linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter is-resized"><img
+					src="http://localhost:8888/utsikt/wp-content/themes/abisko/assets/images/icons/cube.png" alt=""
+					width="48" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"width":"0px","style":"none"}}} -->
+		<div class="wp-block-column"
+			style="border-style:none;border-width:0px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+			<!-- wp:image {"align":"center","width":48,"linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter is-resized"><img
+					src="http://localhost:8888/utsikt/wp-content/themes/abisko/assets/images/icons/cube.png" alt=""
+					width="48" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"width":"0px","style":"none"}}} -->
+		<div class="wp-block-column"
+			style="border-style:none;border-width:0px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+			<!-- wp:image {"align":"center","width":48,"linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter is-resized"><img
+					src="http://localhost:8888/utsikt/wp-content/themes/abisko/assets/images/icons/cube.png" alt=""
+					width="48" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+	<div class="wp-block-columns alignwide">
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"width":"0px","style":"none"}}} -->
+		<div class="wp-block-column"
+			style="border-style:none;border-width:0px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+			<!-- wp:image {"align":"center","width":48,"linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter is-resized"><img
+					src="http://localhost:8888/utsikt/wp-content/themes/abisko/assets/images/icons/cube.png" alt=""
+					width="48" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"width":"0px","style":"none"}}} -->
+		<div class="wp-block-column"
+			style="border-style:none;border-width:0px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+			<!-- wp:image {"align":"center","width":48,"linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter is-resized"><img
+					src="http://localhost:8888/utsikt/wp-content/themes/abisko/assets/images/icons/cube.png" alt=""
+					width="48" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"width":"0px","style":"none"}}} -->
+		<div class="wp-block-column"
+			style="border-style:none;border-width:0px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+			<!-- wp:image {"align":"center","width":48,"linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter is-resized"><img
+					src="http://localhost:8888/utsikt/wp-content/themes/abisko/assets/images/icons/cube.png" alt=""
+					width="48" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"width":"0px","style":"none"}}} -->
+		<div class="wp-block-column"
+			style="border-style:none;border-width:0px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+			<!-- wp:image {"align":"center","width":48,"linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter is-resized"><img
+					src="http://localhost:8888/utsikt/wp-content/themes/abisko/assets/images/icons/cube.png" alt=""
+					width="48" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"width":"0px","style":"none"}}} -->
+		<div class="wp-block-column"
+			style="border-style:none;border-width:0px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+			<!-- wp:image {"align":"center","width":48,"linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter is-resized"><img
+					src="http://localhost:8888/utsikt/wp-content/themes/abisko/assets/images/icons/cube.png" alt=""
+					width="48" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"width":"0px","style":"none"}}} -->
+		<div class="wp-block-column"
+			style="border-style:none;border-width:0px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+			<!-- wp:image {"align":"center","width":48,"linkDestination":"none"} -->
+			<figure class="wp-block-image aligncenter is-resized"><img
+					src="http://localhost:8888/utsikt/wp-content/themes/abisko/assets/images/icons/cube.png" alt=""
+					width="48" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
